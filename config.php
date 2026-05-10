dbb984c71134edc3e0fb15f9aea604bd2a650946
LordDuong <nguyenthaiduong28112005@gmail.com>
2026-05-10 23:28:29 +0700

    Modify database configuration for user input
    Updated database configuration placeholders for user input.

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 
-// Database Configuration
+// Database Configuration, fill in '...'
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root123');
-define('DB_NAME', 'socialnet');
+define('DB_USER', '...'); //Enter your database username into '...'
+define('DB_PASS', '...'); //Enter your database password
+define('DB_NAME', '...'); //Enter your database name
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
